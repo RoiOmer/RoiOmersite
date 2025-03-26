1d7c76a0ac7ccdf2222cdd9081c1f7b85043a021
RoiOmer <omerroye@gmail.com>
2025-03-26 02:41:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -74,5 +74,5 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(() => {
         document.getElementById("loading-screen").style.display = "none";
         document.getElementById("main-container").classList.remove("hidden");
-    }, 1800);
+    }, 2250);
 });
